d961e52733b79f5872fbfee5de2006756cd5a2be
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-27 22:18:19 +0300

    add cancellation module

---
+++ src/cancellation/cancellation.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CancellationService } from './cancellation.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Cancellation from '../entities/cancellation.entity.';
+
+@Module({
+  providers: [CancellationService],
+  imports: [TypeOrmModule.forFeature([Cancellation])],
+})
+export class CancellationModule {}
---
+++ src/cancellation/dto/AddCancellation.dto.ts
@@ -0,0 +1,4 @@
+export default class AddCancellationDto {
+  reason: string;
+  by: string;
+}
