b32227c5e679a33519fedbf67a8a14dc1e48abda
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-04-28 01:33:30 +0300

    delete cancelation controller

--- src/cancellation/cancellation.module.ts
+++ src/cancellation/cancellation.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CancellationService } from './cancellation.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CancellationController } from './cancellation.controller';
 import Cancellation from '../entities/cancellation.entity.';
 
 @Module({
   providers: [CancellationService],
   imports: [TypeOrmModule.forFeature([Cancellation])],
-  controllers: [CancellationController],
 })
 export class CancellationModule {}
