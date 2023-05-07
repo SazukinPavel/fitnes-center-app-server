eb7d9b5127413720783f5832ac8442934d809e19
Pavel Sazukin <sazukinpavelal@gmail.com>
2023-05-07 18:35:29 +0300

    add generate & verify recrete token

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { sign, verify } from 'jsonwebtoken';
 import Auth from '../entities/auth.entity';
+import JwtAuthPayload from '../types/JwtAuthPayload';
 
 @Injectable()
 export class JwtService {
@@ -14,7 +15,25 @@ export class JwtService {
     );
   }
 
+  signRecreateToken(auth: Auth) {
+    return sign(
+      {
+        exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
+        data: { role: auth.role, id: auth.id },
+      },
+      process.env.RECREATE_JWT_KEY,
+    );
+  }
+
+  verifyRecreateToken(token: string) {
+    return verify(token, process.env.RECREATE_JWT_KEY) as {
+      data: JwtAuthPayload;
+    };
+  }
+
   verifyToken(token: string) {
-    return verify(token, process.env.JWT_KEY);
+    return verify(token, process.env.JWT_KEY) as {
+      data: JwtAuthPayload;
+    };
   }
 }
